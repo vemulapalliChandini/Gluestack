33b52fd6ec6f8e8e3c7d3b5857e9f3c424b287ea
vemulapalliChandini <vemulapallidevichandini@gmail.com>
2025-03-11 16:17:29 +0530

    four

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -35,26 +35,6 @@ export default function RootLayout() {
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-        
-
-        
       </Stack></GluestackUIProvider>
   );
 }
